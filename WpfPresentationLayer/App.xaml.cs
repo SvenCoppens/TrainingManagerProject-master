db099e8e47fd15620f42e4d1847f39e3bb978ed7
Sven Coppens <sven.coppens@student.hogent.be>
2020-05-29 02:26:19 +0200

    added Unhandled ExceptionHandler

--- WpfPresentationLayer/App.xaml.cs
+++ WpfPresentationLayer/App.xaml.cs
@@ -13,5 +13,10 @@ namespace WpfPresentationLayer
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occured: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 }
